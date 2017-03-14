ea2984a601255c1b8ccc6ff632d6d8d23e1db318
Danton Nguyen <dantonnguyen88@gmail.com>
2017-03-14 19:43:28 +0000

    exercise_1.rb completed

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,20 @@
 require_relative '../setup'
+# require 'active_recordl'
+
 
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+class Store < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :annual_revenue, numericality: true
+
+end
+
+
+
+Burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
